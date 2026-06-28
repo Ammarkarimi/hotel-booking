cd39919c1960b040a9d85d788f43e404f78183f0
techlit-ai <techlit0010@gmail.com>
2026-06-28 19:17:36 +0530

    fix: Error in card fixed

--- src/components/rooms-client.tsx
+++ src/components/rooms-client.tsx
@@ -241,6 +241,7 @@ export default function RoomsClient() {
                   Delete
                 </Button>
               </div>
+            </div>
             </CardContent>
           </Card>
         ))}
